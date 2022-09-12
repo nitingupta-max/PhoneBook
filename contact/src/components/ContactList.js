a38690be93ead9ee553f7116ea8359b4c06021ba
nitingupta-max <masternitin1234@gmail.com>
2022-09-12 21:57:57 +0530

    material ui

--- contact/src/components/ContactList.js
+++ contact/src/components/ContactList.js
@@ -6,6 +6,35 @@ import ModeEditTwoToneIcon from "@mui/icons-material/ModeEditTwoTone";
 import DeleteForeverTwoToneIcon from "@mui/icons-material/DeleteForeverTwoTone";
 import Checkbox from "@mui/material/Checkbox";
 import { Button } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 const ContactList = () => {
   const [items, setItems] = useState([]);
@@ -46,8 +75,76 @@ const ContactList = () => {
           >
             Multi Delete
           </Button>
-          <table className="table table-striped mt-3">
-            <thead>
+
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>
+                    <CheckBoxOutlineBlankIcon />
+                  </StyledTableCell>
+                  <StyledTableCell align="left">Name</StyledTableCell>
+                  <StyledTableCell align="left">Phone No.</StyledTableCell>
+                  <StyledTableCell align="left">Email</StyledTableCell>
+                  <StyledTableCell align="left">Action</StyledTableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {items.map((contact) => (
+                  <StyledTableRow key={contact._id}>
+                    <StyledTableCell
+                      key={contact._id}
+                      component="th"
+                      scope="row"
+                    >
+                      <Checkbox
+                        onChange={(e) => {
+                          e.preventDefault();
+                          if (e.target.checked) {
+                            setChecked([...checked, contact._id]);
+                          } else {
+                            setChecked([
+                              ...checked.filter((ele) => ele !== contact._id),
+                            ]);
+                          }
+                        }}
+                      />
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {contact.name}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {contact.number}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      {contact.email}
+                    </StyledTableCell>
+                    <StyledTableCell align="left">
+                      <Link to={`/edit/${contact._id}`}>
+                        <Button className="btn">
+                          <ModeEditTwoToneIcon />
+                        </Button>
+                      </Link>{" "}
+                      <Button
+                        className="btn"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          deleteContact(contact._id);
+                          window.location.reload();
+                        }}
+                      >
+                        <DeleteForeverTwoToneIcon />
+                      </Button>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          {/* <table className="table table-striped mt-3"> */}
+          {/* <thead>
               <tr>
                 <th></th>
                 <th>Name</th>
@@ -55,9 +152,9 @@ const ContactList = () => {
                 <th>Email</th>
                 <th>Action</th>
               </tr>
-            </thead>
-            <tbody>
-              {items.map((contact) => (
+            </thead> */}
+          {/* <tbody> */}
+          {/* {items.map((contact) => (
                 <tr key={contact._id}>
                   <Checkbox
                     onChange={(e) => {
@@ -92,9 +189,9 @@ const ContactList = () => {
                     </button>
                   </td>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              ))} */}
+          {/* </tbody> */}
+          {/* </table> */}
         </div>
       </div>
     </>
